c29c4aec5c91302ffe07fe67a373c1352ff82d4b
MarkusHWeisse <147412030+MarkusHWeisse@users.noreply.github.com>
2023-11-08 10:51:27 +0100

    Seeder class implemented.

--- src/Seeder.php
+++ src/Seeder.php
@@ -1,6 +1,18 @@
 <?php
+include_once "OST.php";
+class Seeder {
+    private array $ostList;
 
-class Seeder
-{
+    function __construct(OST ...$ostList) {
+        $this->ostList = $ostList;
+    }
 
+    function getOSTList(): array
+    {
+        return $this->ostList;
+    }
+
+    function setOSTList(array $ostList) {
+        $this->ostList = $ostList;
+    }
 }
