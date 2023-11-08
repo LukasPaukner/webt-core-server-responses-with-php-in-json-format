f6dbf7df0eefe6ec3cdbac08007e6ff0daacf8c4
Lukas Paukner <0055@htl.rennweg.at>
2023-11-08 10:31:50 +0100

    OST getters done.

--- src/OST.php
+++ src/OST.php
@@ -8,4 +8,44 @@ class OST
     public function __construct(private int $ID, private string $name, private string $gameName, private string $erscheinungsDatum, private array $trackList)
     {
     }
+
+    /**
+     * @return int
+     */
+    public function getID(): int
+    {
+        return $this->ID;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGameName(): string
+    {
+        return $this->gameName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getErscheinungsDatum(): string
+    {
+        return $this->erscheinungsDatum;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTrackList(): array
+    {
+        return $this->trackList;
+    }
 }
