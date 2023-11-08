03ecd11c87ca869557003f0f8189bede98dc5ad9
Lukas Paukner <0055@htl.rennweg.at>
2023-11-08 10:20:04 +0100

    OST

--- src/OST.php
+++ src/OST.php
@@ -5,4 +5,7 @@ namespace src;
 class OST
 {
 
+    public function __construct(private int $ID, private string $name, private string $gameName, private string $erscheinungsDatum, private array $trackList)
+    {
+    }
 }
